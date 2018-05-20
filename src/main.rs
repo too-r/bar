399d9838838a42000a4f01d053c8bfd3e4d9189b
too-r <william@huddie.net>
2018-05-20 17:34:03 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -34,53 +34,23 @@ mod tests {
     }
 }
 
-//A screen as seen by Xorg.
 struct Screen {
     name: String,
     xres: String,
     xoffset: String,
 }
 
-//The lemonbar struct, describing the lemonbar process as a child of the main process, and the
-//screen as an X screen.
 struct Lemonbar {
     bar: std::process::Child,
     screen: Screen,
     pow_block: String,
 }
 
-//Function that we use to format output and write it
+// Format for use with lemonbar
 fn add_reset(input: &str) -> String {
     format!("{}%{{B-}}%{{F-}}%{{T-}}", input)
 }
 
-//Returns the number of pacman updates available. Planned: Add pacaur.
-fn arch_updates(config: &Config) -> String {
-    let pacman = Command::new("bash")
-        .args(&["-c", "checkupdates | wc -l"])
-        .output();
-    
-    //Make sure the command output worked.
-    let pacman_out = pacman.unwrap(); //Output only returns an "Ok" type and no error, so we just unwrap.
-
-    let pacaur = Command::new("bash")
-        .args(&["-c", "pacaur -k | wc -l"])
-        .output();
-
-    let pacaur_out = pacaur.unwrap();
-
-    let pacman_result = String::from_utf8(pacman_out.stdout).unwrap();
-    let pacaur_result = String::from_utf8(pacaur_out.stdout).unwrap();
-
-    let package_count = (pacman_result.parse::<i32>().unwrap() + pacaur_result.parse::<i32>().unwrap()).to_string(); //Parse each thing to an i32 and then back to a string again, woo.
-    add_reset(&format!("{}%{{B{}}}%{{F{}}}%{}{}{}",
-                       package_count,
-                       config.colours.bg_sec,
-                       config.colours.fg_col,
-                       config.placeholders.updates,
-                       config.general.update_icon,
-                       config.placeholders.updates))
-}
 
 fn now_playing(config: &Config) -> String {
     let meta_artist = Command::new("bash")
@@ -108,7 +78,6 @@ fn now_playing(config: &Config) -> String {
                        config.placeholders.music))
 }
 
-//Information about the current set of workspaces.
 fn get_ws(screen: &str,
           config: &Config,
           display_count: &i32,
@@ -130,11 +99,11 @@ fn get_ws(screen: &str,
             None => (&config.colours.bg_col, &config.colours.bg_sec),
             Some(i) => {
                 if workspaces[i].visible {
-                    (&config.colours.bg_sec, &config.colours.fg_col) //Has open windows, but is not focused.
+                    (&config.colours.bg_sec, &config.colours.fg_col)
                 } else if workspaces[i].urgent {
-                    (&config.colours.bg_col, &config.colours.hl_col) //Urgent workspace.
+                    (&config.colours.bg_col, &config.colours.hl_col)
                 } else {
-                    (&config.colours.bg_col, &config.colours.fg_sec) //Focused workspace.
+                    (&config.colours.bg_col, &config.colours.fg_sec)
                 }
             }
         };
@@ -153,12 +122,9 @@ fn get_ws(screen: &str,
     add_reset(&result_str)
 }
 
-//What is the time my dude?
 fn get_date(config: &Config) -> String {
     let current = time::now();
     
-    //Check if the current time can be formatted correctly as specified. Planned: Make time
-    //formatting a config thing.
     let current_time_clock = match current.strftime("%H:%M") {
         Ok(fmt) => fmt,
         Err(_) => return String::new(),
@@ -197,7 +163,7 @@ fn get_vol(screen: &str, config: &Config) -> String {
     }
 }
 
-//This is just a placeholder function, we don't use power atm.
+// This is just a placeholder function, we don't use power atm.
 fn get_pow(config: &Config) -> String {
     add_reset(&format!("%{{B{}}}%{{F{}}}{}{}{}",
                        config.colours.bg_sec,
@@ -207,8 +173,8 @@ fn get_pow(config: &Config) -> String {
                        config.placeholders.power))
 }
 
-//This function returns an array of screens. We make it a Vec so if more screens are plugged in, we
-//can account for that.
+// This function returns an array of screens. We make it a Vec so if more screens are plugged in, we
+// can account for that.
 fn get_screens() -> Vec<Screen> {
     let mut screens = Vec::new();
 
@@ -220,11 +186,9 @@ fn get_screens() -> Vec<Screen> {
     let xrandr_str = String::from_utf8_lossy(&xrandr_out.stdout);
     let screen_re = Regex::new("([a-zA-Z0-9-]*) connected .*?([0-9]*)x[^+]*\\+([0-9]*)").unwrap();
     
-    //Iterate through all the capture groups in xrandr_str.
     for caps in screen_re.captures_iter(&xrandr_str) {
-        //For each screen found that matches the regex, push a Screen instance to the Vec.
         screens.push(Screen {
-            name: caps.get(1).unwrap().to_owned().as_str().to_string(), //Messy as hell but it works.
+            name: caps.get(1).unwrap().to_owned().as_str().to_string(),
             xres: caps.get(2).unwrap().to_owned().as_str().to_string(),
             xoffset: caps.get(3).unwrap().to_owned().as_str().to_string(),
         });
@@ -233,18 +197,17 @@ fn get_screens() -> Vec<Screen> {
     screens
 }
 
-//Return a growable array of workspaces
+
 fn i3_get_ws(i3con: &mut I3Connection) -> Vec<i3ipc::reply::Workspace> {
     match i3con.get_workspaces() {
-        Ok(gw) => gw.workspaces, //Workspace Vec was returned correctly
+        Ok(gw) => gw.workspaces,
         Err(_) => {
             *i3con = match I3Connection::connect() {
-                //Make a new i3 connection.
                 Ok(i3c) => i3c,
                 Err(_) => return Vec::new(),
             };
             match i3con.get_workspaces() {
-                Ok(gw) => gw.workspaces, //try to get the workspaces again if the connection to i3 was reestablished
+                Ok(gw) => gw.workspaces,
                 Err(_) => Vec::new(),
             }
         }
@@ -258,11 +221,9 @@ fn main() {
 
         let mut config = config::parse_config();
 
-        let mut lemonbars = Vec::new(); //Vector to contain all the bars we start based on screens.
-
+        let mut lemonbars = Vec::new();
         let mut i3con = I3Connection::connect().unwrap();
         
-        //For each available screen, create a new lemonbar.
         for screen in screens {
             let rect = format!("{}x{}+{}+0",
                                screen.xres,
@@ -288,7 +249,6 @@ fn main() {
                 .spawn()
                 .unwrap();
             
-            //Thread that controls executing lemonbar output.
             let stdout = lemonbar.stdout.take().unwrap();
             thread::spawn(move || unsafe {
                     let _ = Command::new("sh")
@@ -297,7 +257,7 @@ fn main() {
             });
             
             let pow = get_pow(&config);
-            //Collect all currently running bars so that we can use them later.
+            // Collect all currently running bars so that we can use them later.
             let lemonstruct = Lemonbar {
                 bar: lemonbar,
                 screen: screen,
@@ -306,7 +266,6 @@ fn main() {
             lemonbars.push(lemonstruct);
         }
 
-        //Setup a listener to do stuff if monitors are added/removed by Xorg.
         let context = Context::new().unwrap();
         let mut monitor = Monitor::new(&context).unwrap();
         monitor.match_subsystem("drm").unwrap();
@@ -320,7 +279,6 @@ fn main() {
                 curr_time = time::now();
                 config = config::parse_config();
 
-                //If there is a monitor event, restart the bar.
                 if socket.receive_event().is_some() {
                     for lemonbar in &mut lemonbars {
                         let _ = lemonbar.bar.kill();
@@ -352,7 +310,7 @@ fn main() {
                 let _ = stdin.write((&bar_string[..]).as_bytes());
             }
             thread::sleep(Duration::milliseconds(100).to_std().unwrap());
-            continue; //Rerun loop, mainly for the purpose of music.
+            continue;
         }
     }
 }
